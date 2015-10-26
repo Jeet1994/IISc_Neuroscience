43b5b9c897fc39f00ef4b2c15d0e96db4ca9f224
Rajat Saxena <saxenarajat@outlook.com>
2015-10-26 12:07:58 +0530

    updated code for wired calibration

---
+++ piNeuralynx/2wired.py
@@ -0,0 +1,17 @@
+import neuralynxio
+import matplotlib.pyplot as plt
+import numpy as np
+
+def maximumabsolute(datapoints):
+	return np.max(np.abs(datapoints))
+
+channelname = 'CSC1'
+filename = channelname + '.ncs'
+
+csc = neuralynxio.load_ncs(filename)
+
+plt.plot(csc['data'],'b.')
+plt.xlabel('index')
+plt.ylabel('output voltage (microV.)')
+plotname = channelname +'Rawdata.png'
+plt.savefig(plotname)
