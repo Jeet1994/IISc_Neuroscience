dac177255def929a85219a0ac03d3005a94022c4
Rajat Saxena <saxenarajat@outlook.com>
2016-09-25 11:23:15 +0530

    Update angleCalculation.py

--- Miscellaneous/angleCalculation.py
+++ Miscellaneous/angleCalculation.py
@@ -7,25 +7,12 @@ Created on Fri Sep 09 14:22:22 2016
 
 import numpy as np
 
-REFERENCE = [0, 0]
+REFERENCE_VECTOR = np.array([0,200]) - np.array([0,0])
 
-def angleBwLED(redLEDCoords, greenLEDCoords, Reference= REFERENCE):
-    
-    points = np.array([redLEDCoords, greenLEDCoords, Reference])
-    
-    greenREdLEDVector = points[1] - points[0]
-    referenceGreenLEDVector =  points[2] - points[1]
-    referenceRedLEDVector = points[0] - points[2] 
-    
-    angles  = []
-    for v1, v2 in ((greenREdLEDVector, -referenceGreenLEDVector), (referenceGreenLEDVector, -referenceRedLEDVector), (referenceRedLEDVector, -greenREdLEDVector)):
-        cosang = np.dot(v1, v2)
-        sinang = np.linalg.norm(v1) * np.linalg.norm(v2)
-        angles.append(np.arccos(sinang/cosang) * 180 / np.pi)
+def headDirectionAngle(redLEDCoords, greenLEDCoords, referenceVector= REFERENCE_VECTOR):
 
-    if sum(angles) == 180.0:
-        return angles[0]
-    else:
-        raise 
+    greenRedLEDVector = np.array(greenLEDCoords) - np.array(redLEDCoords)
+    angle = np.math.atan2(np.linalg.det([referenceVector,greenRedLEDVector]),np.dot(referenceVector,greenRedLEDVector))
+    print np.degrees(angle)
         
-angle = angleBwLED([2,0],[0,2])
+angle = headDirectionAngle([100,100],[0,200])
