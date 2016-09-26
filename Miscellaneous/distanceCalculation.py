c73f48c3d714919091d3e14e4023976ec42bb5eb
Rajat Saxena <saxenarajat@outlook.com>
2016-09-26 23:20:34 +0530

    Update distanceCalculation.py

--- Miscellaneous/distanceCalculation.py
+++ Miscellaneous/distanceCalculation.py
@@ -4,8 +4,8 @@ import os
 import scipy.io as sio
 import numpy as np
 
-def calculateDistance(red_x_prev, red_y_prev, red_x_next, red_y_next):
-    distance  = np.sqrt((red_x_next - red_x_prev)**2 + (red_y_next - red_y_prev)**2)
+def calculateDistance(x_prev, y_prev, x_next, y_next):
+    distance  = np.sqrt((x_next - x_prev)**2 + (y_next - y_prev)**2)
     return distance 
     
 for filename in os.listdir(os.getcwd()):
@@ -14,14 +14,21 @@ for filename in os.listdir(os.getcwd()):
         distance.append(0)
         
         data = sio.loadmat(filename)
-        red_x = data['red_x'][0]
-        red_y = data['red_y'][0]
+        red_x = list(data['red_x'][0])
+        red_y = list(data['red_y'][0])
+        green_x = list(data['green_x'][0])
+        green_y = list(data['green_y'][0])
         
         for i in range(1,len(red_x)):
-            if (red_x[i-1]==0 or red_y[i-1]==0) and (red_x[i]==0 or red_y[i]==0):
-                distancebwFrames = 640
+            if (red_x[i-1]==-1 or red_x[i]==-1):
+                distancebwFrames = -1
             else:
-                distancebwFrames = (calculateDistance(red_x[i-1], red_y[i-1], red_x[i], red_y[i]))
+                x_prev = red_x[i-1] + green_x[i-1]
+                y_prev = red_y[i-1] + green_y[i-1]
+                x_current = red_x[i] + green_x[i]
+                y_current = red_y[i] + green_y[i]
+                distancebwFrames = calculateDistance(x_prev, y_prev, x_current, y_current)
+                
             distance.append(distancebwFrames)
         
-        sio.savemat(filename, mdict={'red_x':red_x, 'red_y':red_y,'distance':distance})
+        sio.savemat('distance.mat', mdict={'distance':distance})
