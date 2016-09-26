132f67d2eb34ee980a32dceedd8dd225a13eaad0
Rajat Saxena <saxenarajat@outlook.com>
2016-09-26 22:00:13 +0530

    added angle

--- Miscellaneous/angleCalculation.py
+++ Miscellaneous/angleCalculation.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Sep 09 14:22:22 2016
-
 @author: rajat
 """
 
@@ -10,7 +9,6 @@ import numpy as np
 REFERENCE_VECTOR = np.array([0,200]) - np.array([0,0])
 
 def headDirectionAngle(redLEDCoords, greenLEDCoords, referenceVector= REFERENCE_VECTOR):
-
     greenRedLEDVector = np.array(greenLEDCoords) - np.array(redLEDCoords)
     angle = np.math.atan2(np.linalg.det([referenceVector,greenRedLEDVector]),np.dot(referenceVector,greenRedLEDVector))
     print np.degrees(angle)
