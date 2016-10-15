1ecb86f48999b40621457112bf809f8f1ee53270
Rajat Saxena <saxenarajat@outlook.com>
2016-10-15 16:31:16 +0530

    updated plotting script

--- Miscellaneous/plotMapsFunction.m
+++ Miscellaneous/plotMapsFunction.m
@@ -8,20 +8,19 @@ function plotMapsFunction(datum, name)
     %on lower and higher end of colormap
     cmp = [0 0 0; colormap(jet(256)); 1 1 1];
     
+    rateMap = (rateMap/33.31)*1000;
     cmaxRateMap = max(max(rateMap));
-    cminRateMap = min(min(rateMap));
     newcminRateMap = -cmaxRateMap/63;
     rateMapwhite = cmaxRateMap + abs(newcminRateMap);
     rateMap(isnan(rateMap)) = rateMapwhite;
     
+    occMap = occMap*0.03331;
     cmaxOccMap = max(max(occMap));
-    cminOccMap = min(min(occMap));
     newcminOccMap = -cmaxOccMap/63;
     occMapwhite = cmaxOccMap + abs(newcminOccMap);
     occMap(occMap==0) = occMapwhite;
     
     cmaxSpikeMap = max(max(spikeMap));
-    cminSpikeMap = min(min(spikeMap));
     newcminSpikeMap = -cmaxSpikeMap/63;
     spikeMapwhite = cmaxSpikeMap + abs(newcminSpikeMap);
     spikeMapUpdated = spikeMap;
@@ -31,20 +30,23 @@ function plotMapsFunction(datum, name)
     
     figure;
     subplot(1,3,1);
-    title('Rate Map');
     imagesc(rateMap);
+    colormap(cmp);
     colorbar;
+    title('Rate Map(Hz)');
     subplot(1,3,2);
-    title('Spike Map');
     imagesc(spikeMap);
     colormap(cmp);
     colorbar;
+    title('Spike Map');
     subplot(1,3,3);
-    title('Occupancy Map');
     imagesc(occMap);
     colormap(cmp);
     colorbar;
-    [name, r] = strtok(name, '.');
+    title('Occupancy Map(secs)');
+    [name, ~] = strsplit(name, '.UpdatedTimestamps.mat');
+    name = [char(name{1,1})];
+    name = [name '.fig']
     savefig(name);
     close all;
 end
